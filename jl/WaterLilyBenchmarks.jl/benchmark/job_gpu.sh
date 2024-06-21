a8a2506948939eaee49fdbdfd4e79b961d57a3f5
Bernat Font <bernatfontgarcia@gmail.com>
2024-06-21 21:16:05 +0200

    Updated benchmark/job_gpu.sh to new test sizes.

--- jl/WaterLilyBenchmarks.jl/benchmark/job_gpu.sh
+++ jl/WaterLilyBenchmarks.jl/benchmark/job_gpu.sh
@@ -18,6 +18,6 @@ export JULIA_DEPOT_PATH="/home/bsc/bsc021850/.julia-acc"
 
 sh benchmark.sh -b "CuArray" \
 	-c "tgv sphere cylinder donut" \
-	-p "5,6,7,8 3,4,5,6 3,4,5,6 4,5,6,7" \
+	-p "6,7,8,9 3,4,5,6 4,5,6,7 5,6,7,8" \
 	-s "100 100 100 100" \
 	-ft "Float32 Float32 Float32 Float32"
