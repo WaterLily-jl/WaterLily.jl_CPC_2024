bbbf1c1ee463b7daf4352f9fbd878b6550e24f07
weymouth <gabriel.weymouth>
2024-07-17 16:14:15 +0200

    updated davidson routine

--- jl/examples/Davidson.jl
+++ jl/examples/Davidson.jl
@@ -1,25 +1,27 @@
 # Davidson minimizer (should be in Optim, but isn't yet)
 using ForwardDiff: Dual,Tag,value,partials
 using TypedTables
-function davidson(f::F,ax::R,bx::R;tol=1e-2,verbose=true) where {F,R}
-    function eval(f,x)
+function davidson(f::F,ax::R,bx::R;tol=1e-2,verbose=false,itmx=1000) where {F,R}
+    function eval(x)
         T = typeof(Tag(f,R))
         fx = f(Dual{T}(x,one(R)))
         (x=x,f=value(fx),∂=partials(T,fx,1))
     end
-    a,b,Δ = eval(f,ax),eval(f,bx),bx-ax
-    points = [a,b]
-    verbose && println("1: ",a,"\n2: ",b)
-    while abs(Δ) > 2tol
+    a,b,Δ = eval(ax),eval(bx),bx-ax
+    verbose && (points = [a,b])
+    for _ in 1:itmx
         v = a.∂+b.∂-3*(b.f-a.f)/Δ; w = copysign(√(v^2-a.∂*b.∂),Δ)
         x = b.x-Δ*(b.∂+w-v)/(b.∂-a.∂+2w)
-        (x+tol>max(a.x,b.x) || x-tol<min(a.x,b.x)) && (x = 0.5*(a.x+b.x))
-        c = eval(f,x); push!(points,c)
-        verbose && println(length(points),": ",c)
+        c = eval(clamp(x,min(a.x,b.x)+tol,max(a.x,b.x)-tol))
+        verbose && push!(points,c)
         c.f > max(a.f,b.f) && return ErrorException("Process failed")
-        c.∂*a.∂ > 0 ? (a=c) : (b=c)
-        Δ = b.x-a.x
+        (c.f < min(a.f,b.f) ?  c.∂*Δ < 0 : a.f > b.f) ? (a=c) : (b=c)
+        Δ = b.x-a.x; abs(Δ) ≤ 2tol && break
     end
-    (a.f < b.f ? a : b), points |> Table
+    !verbose && return (a.f < b.f ? a : b)
+    points |> Table
 end
-opt,_=davidson(x->-log(x)/x,1.,10.,tol=1e-6); @assert opt.x ≈ exp(1) # test minimizer
+# test minimizer
+@assert davidson(x->(x+3)*(x-1)^2,-2.,2.,tol=1e-6).x ≈ 1
+@assert davidson(x->-log(x)/x,1.,10.,tol=1e-6).x ≈ exp(1)
+@assert davidson(x->cos(x)+cos(3x)/3,0.,1.75π,tol=1e-7).x ≈ π
