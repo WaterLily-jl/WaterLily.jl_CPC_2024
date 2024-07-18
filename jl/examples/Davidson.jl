ae3266be51c35bb93b62f2da22cfaa7e5315f889
weymouth <gabriel.weymouth>
2024-07-18 12:51:03 +0200

    Updated spin example and figures

--- jl/examples/Davidson.jl
+++ jl/examples/Davidson.jl
@@ -1,7 +1,7 @@
-# Davidson minimizer (should be in Optim, but isn't yet)
+# Davidon minimizer (should be in Optim, but isn't yet)
 using ForwardDiff: Dual,Tag,value,partials
 using TypedTables
-function davidson(f::F,ax::R,bx::R;tol=1e-2,verbose=false,itmx=1000) where {F,R}
+function davidon(f::F,ax::R,bx::R;tol=1e-2,verbose=false,itmx=1000) where {F,R}
     function eval(x)
         T = typeof(Tag(f,R))
         fx = f(Dual{T}(x,one(R)))
@@ -12,7 +12,7 @@ function davidson(f::F,ax::R,bx::R;tol=1e-2,verbose=false,itmx=1000) where {F,R}
     for _ in 1:itmx
         v = a.∂+b.∂-3*(b.f-a.f)/Δ; w = copysign(√(v^2-a.∂*b.∂),Δ)
         x = b.x-Δ*(b.∂+w-v)/(b.∂-a.∂+2w)
-        c = eval(clamp(x,min(a.x,b.x)+tol,max(a.x,b.x)-tol))
+        c = eval(clamp(x,min(a.x,b.x)+max(Δ/8,tol),max(a.x,b.x)-max(Δ/8,tol)))
         verbose && push!(points,c)
         c.f > max(a.f,b.f) && return ErrorException("Process failed")
         (c.f < min(a.f,b.f) ?  c.∂*Δ < 0 : a.f > b.f) ? (a=c) : (b=c)
@@ -22,6 +22,6 @@ function davidson(f::F,ax::R,bx::R;tol=1e-2,verbose=false,itmx=1000) where {F,R}
     points |> Table
 end
 # test minimizer
-@assert davidson(x->(x+3)*(x-1)^2,-2.,2.,tol=1e-6).x ≈ 1
-@assert davidson(x->-log(x)/x,1.,10.,tol=1e-6).x ≈ exp(1)
-@assert davidson(x->cos(x)+cos(3x)/3,0.,1.75π,tol=1e-7).x ≈ π
+@assert davidon(x->(x+3)*(x-1)^2,-2.,2.,tol=1e-6).x ≈ 1
+@assert davidon(x->-log(x)/x,1.,10.,tol=1e-6).x ≈ exp(1)
+@assert davidon(x->cos(x)+cos(3x)/3,0.,1.75π,tol=1e-7).x ≈ π
--- jl/examples/SpinCylOptim.jl
+++ jl/examples/SpinCylOptim.jl
@@ -1,17 +1,18 @@
 using WaterLily,StaticArrays
 
 # Spinning cylinder control simulation
-using WaterLily: norm2                   # vector length
-rot(θ) = [cos(θ) -sin(θ); sin(θ) cos(θ)] # rotation matrix
+using WaterLily: norm2                     # vector length
+rot(θ) = SA[cos(θ) -sin(θ); sin(θ) cos(θ)] # rotation matrix
 function drag_control_sim(ξ;D=96,Re=500,d_D=0.15f0,g_D=0.05f0)
     # set up big cylinder
     C,R,U = SA[2D,0],D÷2,1
-    big = AutoBody((x,t)->norm2(x-C)-R)
+    big = AutoBody((x,t)->norm2(x-C)-R)         # circle sdf
     # set up small control cylinder
     r = d_D*R; c = C+(R+r+g_D*D)*SA{Float32}[1/2,√3/2]
-    small = AutoBody((x,t)->norm2(x)-r,(x,t)->rot(ξ[]*U*t/r)*(x-c))
+    small = AutoBody((x,t)->norm2(x)-r,         # circle sdf
+                     (x,t)->rot(ξ*U*t/r)*(x-c)) # center & spin!
     # set up simulation
-    Simulation((6D,2D),(U,0),D;ν=U*D/Re,body=big+small,T=typeof(ξ[]))
+    Simulation((6D,2D),(U,0),D;ν=U*D/Re,body=big+small,T=typeof(ξ))
 end
 
 # Measure propulsive power (Thrust*U) normalized by the propulsor powering scale dξ³U³
@@ -25,7 +26,7 @@ function cost(ξ,t_end=2;d_D=0.15,remeasure=false)
     sim_step!(sim,t_end;remeasure)
     -scaled_power(sim,d_D*ξ^3)
 end
-points = davidson(cost,3.,8.,tol=5e-2,verbose=true)
+points = davidon(cost,3.,8.,tol=1e-2,verbose=true)
 
 # Plot optimization points
 using Plots,LaTeXStrings
@@ -46,14 +47,18 @@ Plots.default(
     labelfontsize = 14,
 )
 
-points = filter(a->a.f<0,points) # remove drag cases\
-mine = palette([:cyan, :blue], 5)
-scatter(points.x, points.f/opt.f, xlabel=L"$\xi$",ylabel=L"$c_p/\max(c_p)$", ylims=(0,1.1), label=nothing,
-        zcolor = eachindex(points), markercolor=mine, colorbar_title="Iteration", size=(600,600))
+opt = argmin(a->a.f,points)            # optimimum point
+mine = palette([:red, :green], 6)
+plot(xlabel=L"$\xi$",ylabel=L"$C_P/\max(C_P)$", ylims=(0,1.1),size=(600,600))
+for i in 2:6
+    scatter!([points.x[i]], [points.f[i]/opt.f],label=nothing,c=mine[i])
+    plot!(points.x[i] .+ [-0.1,0.1], (points.f[i] .+ points.∂[i]*[-0.1,0.1])/opt.f,label="it=$i",c=mine[i])
+end
+plot!()
 savefig("SpinOptim.pdf")
 
 # Plot the scaled power history
-opt = (x = 6.252768481467935, f = -0.0007056811885623282, ∂ = 0.0956334509946055)
+opt = (x = 6.26364389415871, f = -0.000705763068600175, ∂ = 2.869126765532603e-7)
 function history(ξ,duration=3;d_D=0.15,remeasure=false)
     sim = drag_control_sim(ξ;d_D)
     data = map(range(0,duration,300)) do tᵢ
@@ -64,11 +69,11 @@ function history(ξ,duration=3;d_D=0.15,remeasure=false)
 end
 histOpt,sim = history(opt.x);
 hist3,_ = history(3.0);
-plot(hist3,label=L"$\xi=3$")
-scatter!(filter(a->abs(a.t-2)<3e-3,hist3),c=1,label=nothing)
-plot!(histOpt,label=L"$\xi=6.253$",ylims=(-0.4,0.1),c=2)
-scatter!(filter(a->abs(a.t-2)<3e-3,histOpt),c=2,label=nothing)
-plot!(xlabel=L"$tU/D$",ylabel=L"$c_p$",size=(600,600))
+plot(hist3,label=L"$\xi=3$",c=:red)
+scatter!(filter(a->abs(a.t-2)<3e-3,hist3),c=:red,label=nothing)
+plot!(histOpt,label=L"$\xi=6.26$",ylims=(-0.4,0.1),c=:green)
+scatter!(filter(a->abs(a.t-2)<3e-3,histOpt),c=:green,label=nothing)
+plot!(xlabel=L"$tU/D$",ylabel=L"$C_P$",size=(600,600))
 savefig("SpinCylHist.pdf")
 
 # Plot the flow & circles
