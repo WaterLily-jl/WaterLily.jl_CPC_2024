8e45b2fc9ba0d951b647c694feec464b874b64b3
Bernat Font <bernatfontgarcia@gmail.com>
2025-02-12 19:53:31 +0100

    Updated profile/job_mn5.sh

--- jl/WaterLilyBenchmarks.jl/profile/job_mn5.sh
+++ jl/WaterLilyBenchmarks.jl/profile/job_mn5.sh
@@ -8,12 +8,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:1
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 
 export JULIA_DEPOT_PATH="$HOME/.julia-acc"
 
 sh profile.sh \
 	-b "CuArray" \
 	-c "tgv sphere cylinder" \
-	-p "8 5 6"
+	-p "8 5 6" \
+	-r "1"
 
