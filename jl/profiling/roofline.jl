494cf3f1551ff83c74873ed2e71a53f57d33611d
Bernat Font <bernatfontgarcia@gmail.com>
2025-02-25 13:28:48 +0100

    More info the the roofline figure.

--- jl/profiling/roofline.jl
+++ jl/profiling/roofline.jl
@@ -1,6 +1,6 @@
 # Post-processing Nsight Compute results to plot roofline model for different kernels
 
-using Plots
+using Plots, Printf
 
 
 fontsize = 11
@@ -17,41 +17,64 @@ Plots.default(
     tickfontsize = fontsize,
     labelfontsize = fontsize,
 )
+markersize = 5
+fc = RGBA(0.5, 0.5, 0.5, 0.5)
 
 const mem_bandwidth = 256e9
 const max_perf_fp32 = 7004e9
 const min_AI_fp32 = 27.38
 const max_perf_fp64 = 109e9
 const min_AI_fp64 = 0.43
-const x_lims = (1e-2, 1e4)
+const x_lims = (1e-1, 1e2)
 const y_lims = (1e9, 1e14)
 
-y_mem(x) = mem_bandwidth*x/1e12
+y_mem_normed(x) = mem_bandwidth*x/1e12
+bandwidth_pc(x::Tuple{T,T} where T<:Number) = x[2]/x[1]/mem_bandwidth*100
+norm_point(x::Tuple{T,T} where T<:Number) = (x[1], x[2]/1e12)
 
-kern_395_fp32_fine = (0.54, 128052e6/1e12)
-kern_451_fp32_fine = (0.61, 81288e6/1e12)
-kern_451_fp64_fine = (0.15, 20322e6/1e12)
-kern_395_fp32_coarse = (0.55, 116743e6/1e12)
-kern_451_fp32_coarse = (0.73, 81190e6/1e12)
-kern_451_fp64_coarse = (0.18, 20047e6/1e12)
+kern_395_fp32_G1 = (0.62, 93064e6)
+kern_451_fp32_G1 = (0.97, 68071e6)
+kern_451_fp64_G1 = (0.24, 17018e6)
+kern_395_fp32_G2 = (0.55, 116743e6)
+kern_451_fp32_G2 = (0.73, 81190e6)
+kern_451_fp64_G2 = (0.18, 20047e6)
+kern_395_fp32_G3 = (0.54, 128052e6)
+kern_451_fp32_G3 = (0.61, 81288e6)
+kern_451_fp64_G3 = (0.15, 20322e6)
+
 
 p = plot(ylabel="Performance [TFLOP/s]", xlabel="Arithmetic Intensity [FLOP/byte]",
     ylims=y_lims./1e12, xlims=x_lims, yminorgrid=true, xminorgrid=true,
     yaxis=:log10, xaxis=:log10, legend=:bottomright, background_color_legend=RGBA{Float64}(1, 1, 1, 0.7)
 )
 # fancylogscale!(p)
-plot!(p, y_mem, linewidth=2, color=:darkblue, primary=false)
+plot!(p, y_mem_normed, linewidth=2, color=:darkblue, primary=false)
 plot!(p, [min_AI_fp32, x_lims[2]], [max_perf_fp32/1e12, max_perf_fp32/1e12], linewidth=2, color=:black, primary=false)
 plot!(p, [x_lims[1], min_AI_fp32], [max_perf_fp32/1e12, max_perf_fp32/1e12], linewidth=2, color=:black, linestyle=:dash, primary=false)
 plot!(p, [min_AI_fp64, x_lims[2]], [max_perf_fp64/1e12, max_perf_fp64/1e12], linewidth=2, color=:grey, primary=false)
 plot!(p, [x_lims[1], min_AI_fp64], [max_perf_fp64/1e12, max_perf_fp64/1e12], linewidth=2, color=:grey, linestyle=:dash, primary=false)
 
-scatter!(p, kern_395_fp32_coarse, ms=7, ma=1, color=:green, label="kernel_395 FP32 G2", markershape=:diamond)
-scatter!(p, kern_451_fp32_coarse, ms=7, ma=1, color=:blue, label="kernel_451 FP32 G2", markershape=:diamond)
-scatter!(p, kern_451_fp64_coarse, ms=7, ma=1, color=:blue, label="kernel_451 FP64 G2")
+scatter!(p, norm_point(kern_395_fp32_G1), label=@sprintf("project!   0.44M FP32 %d", bandwidth_pc(kern_395_fp32_G1))*"%",
+    ms=markersize, ma=1, color=:lightyellow, markershape=:diamond, alpha=fc)
+scatter!(p, norm_point(kern_395_fp32_G2), label=@sprintf("project!   3.54M FP32 %d", bandwidth_pc(kern_395_fp32_G2))*"%",
+    ms=markersize, ma=1, color=:green, markershape=:diamond, alpha=fc)
+scatter!(p, norm_point(kern_395_fp32_G3), label=@sprintf("project!  28.31M FP32 %d", bandwidth_pc(kern_395_fp32_G3))*"%",
+    ms=markersize, ma=1, color=:lightgreen, markershape=:diamond, alpha=fc)
 
-scatter!(p, kern_395_fp32_fine, ms=7, ma=1, color=:lightgreen, label="kernel_395 FP32", markershape=:diamond)
-scatter!(p, kern_451_fp32_fine, ms=7, ma=1, color=:lightblue, label="kernel_451 FP32", markershape=:diamond)
-scatter!(p, kern_451_fp64_fine, ms=7, ma=1, color=:lightblue, label="kernel_451 FP64")
+scatter!(p, norm_point(kern_451_fp32_G1), label=@sprintf("convdiff!  0.44M FP32 %d", bandwidth_pc(kern_451_fp32_G1))*"%",
+    ms=markersize, ma=1, color=:cyan, markershape=:diamond)
+scatter!(p, norm_point(kern_451_fp32_G2), label=@sprintf("convdiff!  3.54M FP32 %d", bandwidth_pc(kern_451_fp32_G2))*"%",
+    ms=markersize, ma=1, color=:blue, markershape=:diamond)
+scatter!(p, norm_point(kern_451_fp32_G3), label=@sprintf("convdiff! 28.31M FP32 %d", bandwidth_pc(kern_451_fp32_G3))*"%",
+    ms=markersize, ma=1, color=:lightblue, markershape=:diamond)
+
+scatter!(p, norm_point(kern_451_fp64_G1), label=@sprintf("convdiff!  0.44M FP64 %d", bandwidth_pc(kern_451_fp64_G1))*"%",
+    ms=markersize, ma=1, color=:cyan)
+scatter!(p, norm_point(kern_451_fp64_G2), label=@sprintf("convdiff!  3.54M FP64 %d", bandwidth_pc(kern_451_fp64_G2))*"%",
+    ms=markersize, ma=1, color=:blue)
+scatter!(p, norm_point(kern_451_fp64_G3), label=@sprintf("convdiff! 28.31M FP64 %d", bandwidth_pc(kern_451_fp64_G3))*"%",
+    ms=markersize, ma=1, color=:lightblue)
+
+
 
 savefig(p, joinpath(string(@__FILE__), "../../../tex/img/roofline_cylinder.pdf"))
