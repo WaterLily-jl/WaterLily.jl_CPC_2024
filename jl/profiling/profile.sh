7867a42aa6cc4fad4650629a00dbab376480aaab
Bernat Font <bernatfontgarcia@gmail.com>
2025-06-01 13:02:17 +0200

    New profiling figures with v1.4.0

--- jl/profiling/profile.sh
+++ jl/profiling/profile.sh
@@ -8,9 +8,12 @@ DATA_DIR=$THIS_DIR"/data"
 PLOT_DIR=$THIS_DIR"/plots"
 TEX_IMG_DIR=$THIS_DIR"/../../tex/img"
 
+# Run and post-process profiling
+sh $WB_DIR/profile.sh -c "tgv sphere cylinder" -p "8 5 6" -s 1000 -r 2 -dd $DATA_DIR -pd $PLOT_DIR
+# Run profiling
+# sh $WB_DIR/profile.sh -c "tgv sphere cylinder" -p "8 5 6" -s 1000 -r 1 -dd $DATA_DIR -pd $PLOT_DIR
 # Post-process profiling
 # sh $WB_DIR/profile.sh -c "tgv sphere cylinder" -p "8 5 6" -s 1000 -r 0 -dd $DATA_DIR -pd $PLOT_DIR
-sh $WB_DIR/profile.sh -c "tgv sphere cylinder" -p "8 5 6" -s 1000 -r 1 -dd $DATA_DIR -pd $PLOT_DIR
 
 # Copy plots to TeX folder
 cp $PLOT_DIR/* $TEX_IMG_DIR
